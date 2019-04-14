f9138a064bf3e06fa320fdaee4b170e1e2e5d851
Katherine Lewis <kale8432@colorado.edu>
2019-04-13 20:33:46 -0600

    Styled main header

--- App Files/src/App/App.js
+++ App Files/src/App/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
 import './App.scss';
+import Data from '../Data/Data.js';
+import Score from '../Score/Score.js';
+import QuestionCount from '../QuestionCount/QuestionCount.js';
+import StartPopUp from '../StartPopUp/PopUp.js';
+import QuestionCard from '../QuestionCard/QuestionCard.js';
 
 export default class App extends Component {
   constructor(){
@@ -18,11 +22,12 @@ export default class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1>
+          <h1 className="App-title">
             CodeSnippits
           </h1>
+          <h3 className="App-instructions">Welcome! Analyze the code snippet. Select the correct answer. Submit to W!N.</h3>
         </header>
+        <QuestionCard key={this.state.questionSetKey} question={this.state.codeSnippetQ}/>
       </div>
     );
   }
--- App Files/src/index.js
+++ App Files/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
+import Normalize from "./normalize.scss"
+import Styles from './index.scss';
 import App from './App/App.js';
 import * as serviceWorker from './serviceWorker';
 
